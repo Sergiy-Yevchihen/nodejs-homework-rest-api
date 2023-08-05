d76efce4aafadc41a2e76c7cd5753dc97cf95a3b
Serhii Yevchishen <magodza06@gmail.com>
2023-08-05 14:32:27 +0300

    add fix updateContact

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -37,34 +37,37 @@ const removeContact = async (req, res) => {
   });
 };
 
-// const updateContact = async (req, res) => {
-//   if (Object.keys(req.body).length === 0) {
-//     throw HttpError(400, "missing fields");
-//   }
-//   const { id } = req.params;
-//   const result = await contacts.updateContact(id, req.body);
-//   res.status(200).json(result);
-// };
 const updateContact = async (req, res) => {
   if (Object.keys(req.body).length === 0) {
     throw HttpError(400, "missing fields");
   }
-
   const { id } = req.params;
-  const contact = await contacts.getContactById(id);
-
-  if (!contact) {
-    throw HttpError(404, "Not found");
-  }
-
-  const updatedContact = {
-    ...contact,
-    ...req.body,
-  };
-
-  await contacts.updateContact(id, updatedContact);
-  res.status(200).json(updatedContact);
+  const result = await contacts.updateContact(id, req.body);
+   if (!result) {
+     throw HttpError(404, "Not found");
+   }
+  res.status(200).json(result);
 };
+// const updateContact = async (req, res) => {
+//   if (Object.keys(req.body).length === 0) {
+//     throw HttpError(400, "missing fields");
+//   }
+
+//   const { id } = req.params;
+//   const contact = await contacts.getContactById(id);
+
+//   if (!contact) {
+//     throw HttpError(404, "Not found");
+//   }
+
+//   const updatedContact = {
+//     ...contact,
+//     ...req.body,
+//   };
+
+//   await contacts.updateContact(id, updatedContact);
+//   res.status(200).json(updatedContact);
+// };
 
 module.exports = {
   listContacts: ctrlWrapper(listContacts),
