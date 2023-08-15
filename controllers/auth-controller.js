743a4eb3208fd8502f4c4fd49ebcb9f66c39540b
Serhii Yevchishen <magodza06@gmail.com>
2023-08-15 13:23:34 +0300

    add hw05-avatars

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,11 +1,17 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 const { HttpError } = require("../helpers");
 const { ctrlWrapper } = require("../decorators");
 
 const { SECRET_KEY } = process.env;
+
+const avatarDir = path.resolve("public", "avatars");
 
 
 const register = async (req, res) => {
@@ -18,17 +24,20 @@ const register = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarUrl = gravatar.url(email);
 
   const newUser = await User.create({
     ...req.body,
     password: hashPassword,
     subscription,
+    avatarUrl,
   });
 
   res.status(201).json({
     user: {
       email: newUser.email,
       subscription: newUser.subscription,
+      avatarUrl: newUser.avatarUrl,
     },
   });
 };
@@ -102,10 +111,46 @@ const updateSubscription = async (req, res) => {
   res.json({ result });
 };
 
+const updateAvatar = async (req, res, next) => {
+  if (!req.file) {
+    throw HttpError(400, "Avatar must be provided");
+  }
+
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+
+  await Jimp.read(tempUpload)
+    .then((avatar) => {
+      return avatar
+        .resize(250, 250) 
+        .quality(60) 
+        .write(tempUpload); 
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+  const fileName = `${_id}_${originalname}`;
+
+  const publicUpload = path.join(avatarDir, fileName);
+
+  await fs.rename(tempUpload, publicUpload);
+
+  const avatarUrl = path.join("avatars", fileName);
+
+  await User.findByIdAndUpdate(_id, { avatarUrl });
+
+  res.json({
+    avatarUrl,
+  });
+};
+
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
   updateSubscription: ctrlWrapper(updateSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,5 +1,6 @@
 const isValidId = require("./isValidId");
 const checkBody = require("./checkBody");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
-module.exports = { isValidId, checkBody, authenticate };
+module.exports = { isValidId, checkBody, authenticate, upload, };
